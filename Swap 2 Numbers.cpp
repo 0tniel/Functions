c7ef3661484a3abcef530e7bf39bb67e7c18fe47
0tniel <jhiradotniel@gmail.com>
2024-09-02 15:37:23 +0530

    Add files via upload

---
+++ Swap 2 Numbers.cpp
@@ -0,0 +1,30 @@
+// Otniel
+// ENTC A3
+// 23070123069
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter value for a: ";
+    cin >> a;
+    cout << "Enter value for b: ";
+    cin >> b;
+    swap(&a, &b);
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+    return 0;
+}
+/*
+Output
+*/
+
